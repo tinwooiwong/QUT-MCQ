bc5861f02952ec63e77bd1e5ca3621f6d16f31b5
Wong Tin Wooi <tinwooi.wong@gmail.com>
2019-09-24 22:17:13 +1000

    Initial Commit - Completed with marks checker

---
+++ QUT MCQ/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace QUT_MCQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.log = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedFile = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.log.Location = new System.Drawing.Point(64, 29);
+            this.log.Multiline = true;
+            this.log.Name = "textBox1";
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(626, 283);
+            this.log.TabIndex = 0;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Enabled = false;
+            this.startBtn.Location = new System.Drawing.Point(63, 344);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(627, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start Generating";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Log";
+            // 
+            // selectedFile
+            // 
+            this.selectedFile.Location = new System.Drawing.Point(64, 318);
+            this.selectedFile.Name = "selectedFile";
+            this.selectedFile.ReadOnly = true;
+            this.selectedFile.Size = new System.Drawing.Size(315, 20);
+            this.selectedFile.TabIndex = 4;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(385, 318);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(305, 20);
+            this.browseBtn.TabIndex = 5;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(64, 374);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(626, 23);
+            this.closeBtn.TabIndex = 6;
+            this.closeBtn.Text = "Exit Program";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text files | *.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 431);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.selectedFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.log);
+            this.Name = "Form1";
+            this.Text = "QUT MCQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox selectedFile;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ QUT MCQ/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.xml;
+using System.IO;
+
+namespace QUT_MCQ
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+        private AcroFields fields;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            log.Text = "Please browse for a text file to begin..." + Environment.NewLine;
+        }
+
+        private void FillForm()
+        {
+            int totalMarks = 0;
+            int accumulatedMarks = 0;
+            string pdfTemplate = "C:\\Users\\Zhi On\\Desktop\\WTW\\QUT_MCQ_CS.pdf";
+            string newFile = "C:\\Users\\Zhi On\\Desktop\\WTW\\QUT_MCQ_CS_NEW.pdf";
+            PdfReader pdfReader = new PdfReader(pdfTemplate);
+            PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileStream(newFile, FileMode.Create));
+            fields = pdfStamper.AcroFields;
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] splittedline = line.Split(' ');
+
+                    switch (splittedline[0])
+                    {
+                        case "Surname":
+                            fillSurname(splittedline[1]);
+                            break;
+                        case "Initials":
+                            fillInitials(splittedline[1]);
+                            break;
+                        case "SubjectCode":
+                            fillSubjectCode(splittedline[1]);
+                            break;
+                        case "StudentNumber":
+                            fillStudentNumber(splittedline[1]);
+                            break;
+                        case "Codes":
+                            fillCodes(splittedline[1]);
+                            break;
+                        case "TotalMarks":
+                            totalMarks = Int32.Parse(splittedline[1]);
+                            break;
+                        default:
+                            if (Int32.TryParse(splittedline[0], out int number))
+                            {
+                                fillQuestionAnswer(splittedline);
+                                accumulatedMarks += Int32.Parse(splittedline[2]);
+                            } else
+                            {
+                                log.Text = log.Text + DateTime.Now + "-- INVALID INPUT -- " + line + Environment.NewLine;
+                            }
+                            break;
+                    }
+                }
+                sr.Close();
+            }
+
+            pdfStamper.FormFlattening = true;
+            pdfStamper.Close();
+
+            log.Text = log.Text + DateTime.Now + " -- Generating Completed for " + fields.GetField("Subject Code") + Environment.NewLine;
+
+            if(totalMarks != accumulatedMarks)
+            {
+                log.Text = log.Text + DateTime.Now + "WARNING -- ACCUMULATED MARKS DOES NOT EQUAL TO THE TOTAL MARKS SET! PLEASE CHECK AGAIN!";
+            } else
+            {
+                log.Text = log.Text + DateTime.Now + "ACCUMULATED MARKS MATCHED THE TOTAL MARKS SET";
+            }
+        }
+
+        private void fillSurname(string value)
+        {
+            char[] surname = value.ToCharArray();
+
+            log.Text = log.Text + DateTime.Now + " -- Generating Surname" + Environment.NewLine;
+
+            fields.SetField("Surname", value);
+            for (int i = 0; i < surname.Length; i++)
+            {
+                fields.SetField("Surname" + (i + 1), surname[i].ToString());
+            }
+            log.Text = log.Text + DateTime.Now + " -- Generating Surname Completed" + Environment.NewLine;
+        }
+
+        private void fillInitials(string value)
+        {
+            char[] initials = value.ToCharArray();
+
+            log.Text = log.Text + DateTime.Now + " -- Generating Initials" + Environment.NewLine;
+            fields.SetField("Initials", value);
+            for (int i = 0; i < initials.Length; i++)
+            {
+                fields.SetField("Initials" + (i + 1), initials[i].ToString());
+            }
+            log.Text = log.Text + DateTime.Now + " -- Generating Initials Completed" + Environment.NewLine;
+        }
+
+        private void fillSubjectCode(string value)
+        {
+            char[] subjectCode = value.ToCharArray();
+
+            log.Text = log.Text + DateTime.Now + " -- Generating Subject Code" + Environment.NewLine;
+            fields.SetField("Subject Code", value);
+            for (int i = 0; i < subjectCode.Length; i++)
+            {
+                fields.SetField("SubjectCode" + (i + 1), subjectCode[i].ToString());
+            }
+            log.Text = log.Text + DateTime.Now + " -- Generating Subject Code Completed" + Environment.NewLine;
+        }
+
+        private void fillStudentNumber(string value)
+        {
+            char[] studentNumber = value.ToCharArray();
+
+            log.Text = log.Text + DateTime.Now + " -- Generating Student Number" + Environment.NewLine;
+            fields.SetField("Student Number", value);
+            for (int i = 0; i < studentNumber.Length; i++)
+            {
+                fields.SetField("StudentNumber" + (i + 1), studentNumber[i].ToString());
+            }
+            log.Text = log.Text + DateTime.Now + " -- Generating Student Number Completed" + Environment.NewLine;
+        }
+
+        private void fillCodes(string value)
+        {
+            char[] codes = value.ToCharArray();
+
+            log.Text = log.Text + DateTime.Now + " -- Generating Codes" + Environment.NewLine;
+            fields.SetField("Codes", value);
+            for (int i = 0; i < codes.Length; i++)
+            {
+                fields.SetField("Codes" + (i + 1), codes[i].ToString());
+            }
+            log.Text = log.Text + DateTime.Now + " -- Generating Codes Completed";
+        }
+
+        private void fillQuestionAnswer(string[] value)
+        {
+            log.Text = log.Text + DateTime.Now + "-- Generating Question Answer for Q" + value[0] + Environment.NewLine;
+            //fields.SetField("Q" + value[0], value[1].ToUpper());
+            fields.SetField("Q" + value[0] + value[1].ToUpper(), "Yes");
+            log.Text = log.Text + DateTime.Now + "-- Generating Question Answer Completed for Q" + value[0] + Environment.NewLine;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            log.Text = log.Text + DateTime.Now + "-- Start Generating" + Environment.NewLine;
+            FillForm();
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            log.Text = log.Text + DateTime.Now + " -- Browsing File" + Environment.NewLine;
+
+            DialogResult dialogResult = this.openFileDialog1.ShowDialog();
+            
+            if(dialogResult == DialogResult.OK)
+            {
+                log.Text = log.Text + DateTime.Now + " -- " + this.openFileDialog1.FileName + " has been selected" + Environment.NewLine;
+                this.selectedFile.Text = this.openFileDialog1.FileName;
+                filePath = this.openFileDialog1.FileName;
+                this.startBtn.Enabled = true;
+            } else if(dialogResult == DialogResult.Cancel)
+            {
+                log.Text = log.Text + DateTime.Now + " -- Browsing File Operation has been cancelled" + Environment.NewLine;
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
